639d8e90547c3203d477b01764e966ae0d610284
Matus Olejnik <matus.olejnik@gmail.com>
2022-04-14 04:29:55 +0200

    feat: add encryption test

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,5 +1,9 @@
 import org.junit.jupiter.api.Test;
 
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import java.util.Objects;
+
 public class BaseTest {
 
     @Test
@@ -31,6 +35,28 @@ public class BaseTest {
                 "Test 3",
                 1,
                 TestResultTypeEnu.CORRECT
+        );
+        System.out.println(tr);
+    }
+
+
+    @Test
+    void testEncryptAndDecrypt3b() throws Exception {
+        String input = "aaaa FWANFJ 4782 @$&(";
+
+        CryptoHelperImpl cryptoHelper = new CryptoHelperImpl();
+
+        SecretKey key = cryptoHelper.generateKey(128);
+        IvParameterSpec ivParameterSpec = cryptoHelper.generateIv();
+        String algorithm = "AES/CBC/PKCS5Padding";
+        String cipherText = cryptoHelper.encrypt(algorithm, input, key, ivParameterSpec);
+        String plainText = cryptoHelper.decrypt(algorithm, cipherText, key, ivParameterSpec);
+
+        TestResult tr = new TestResult(
+                "74144",
+                "Test encryption",
+                3,
+                Objects.equals(input, plainText) ? TestResultTypeEnu.CORRECT : TestResultTypeEnu.FAIL
         );
         System.out.println(tr);
     }
